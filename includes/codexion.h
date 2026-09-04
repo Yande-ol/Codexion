3fcf26abc0409debe4fba32d44a8e7d2e6152b39
Yande-ol <yaancarlos8@icloud.com>
2026-09-04 14:34:05 +0100

    feat: add main codexion header with external dependencies

---
+++ includes/codexion.h
@@ -0,0 +1,15 @@
+#ifndef CODEXION_H
+# define CODEXION_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+# include "constants.h"
+# include "structures.h"
+
+
+#endif
