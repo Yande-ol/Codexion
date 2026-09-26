114e6019ae62590bdd72aca8a79c98a17eae3eb3
Yande-ol <yaancarlos8@icloud.com>
2026-09-26 04:26:39 +0100

    feat: implement simulation resources and mutexes initialization

---
+++ src/sync/init_sync.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_sync.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Yande-ol <Yande-ol@student.42porto.com>    #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026-09-25 23:24:23 by Yande-ol          #+#    #+#             */
+/*   Updated: 2026-09-25 23:24:23 by Yande-ol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+static int	init_dongles(t_data *data)
+{
+	int	i;
+
+	data->dongles = malloc(sizeof(t_dongle) * data->num_coders);
+	if (!data->dongles)
+		return (0);
+	i = 0;
+	while (i < data->num_coders)
+	{
+		data->dongles[i].id = i;
+		data->dongles[i].is_in_use = 0;
+		data->dongles[i].last_released_time = 0;
+		if (pthread_mutex_init(&data->dongles[i].mutex, NULL) != 0)
+			return (0);
+		if (pthread_cond_init(&data->dongles[i].cond, NULL) != 0)
+			return (0);
+		if (!heap_init(&data->dongles[i].queue, data->num_coders,
+				data->scheduler_type))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	init_coders(t_data *data)
+{
+	int	i;
+
+	data->coders = malloc(sizeof(t_coder) * data->num_coders);
+	if (!data->coders)
+		return (0);
+	i = 0;
+	while (i < data->num_coders)
+	{
+		data->coders[i].id = i + 1;
+		data->coders[i].compiles_count = 0;
+		data->coders[i].last_compile_start = 0;
+		data->coders[i].data = data;
+		data->coders[i].left_dongle = &data->dongles[i];
+		data->coders[i].right_dongle = &data->dongles[(i + 1)
+			% data->num_coders];
+		if (pthread_mutex_init(&data->coders[i].meal_mutex, NULL) != 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	init_simulation_data(t_data *data)
+{
+	data->sim_stopped = 0;
+	data->sim_start_time = 0;
+	data->dongles = NULL;
+	data->coders = NULL;
+	if (pthread_mutex_init(&data->stop_mutex, NULL) != 0)
+		return (0);
+	if (pthread_mutex_init(&data->log_mutex, NULL) != 0)
+		return (0);
+	if (!init_dongles(data))
+		return (0);
+	if (!init_coders(data))
+		return (0);
+	return (1);
+}
